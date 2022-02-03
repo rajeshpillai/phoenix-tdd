a18457e5a0d744b83071e3eabe18c28254d1067f
rajeshpillai <pillai.rajesh@gmail.com>
2022-02-03 15:34:12 +0530

    Basic test setup and custom config for feature test

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ import Config
 # Run `mix help test` for more information.
 config :chatter, Chatter.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "root123",
   database: "chatter_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox,
@@ -18,7 +18,7 @@ config :chatter, Chatter.Repo,
 config :chatter, ChatterWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4002],
   secret_key_base: "sQvIhmhQRVfRcFqgRHAFdeRUVE89otNDW2IkF0OMlHoMFcwf019BxkZM4qy0kOjb",
-  server: false
+  server: true  # rp: so that server is run during test
 
 # In test we don't send emails.
 config :chatter, Chatter.Mailer, adapter: Swoosh.Adapters.Test
@@ -28,3 +28,8 @@ config :logger, level: :warn
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
+
+# Use Ecto's sandbox in test
+config :chatter, :sql_sandbox, true
+
+config :wallaby, driver: Wallaby.Chrome
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,8 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Chatter.Repo, :manual)
+
+# Start Wallaby
+{:ok, _} = Application.ensure_all_started(:wallaby)
+
+# Basse URL
+Application.put_env(:wallaby, :base_url, ChatterWeb.Endpoint.url())
