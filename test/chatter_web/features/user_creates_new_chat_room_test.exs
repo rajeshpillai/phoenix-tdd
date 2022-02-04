f99ad8add7da97691413bb55f2a4c8b9c7a9050e
rajeshpillai <pillai.rajesh@gmail.com>
2022-02-04 17:38:21 +0530

    Auth test: create factory and modify existing test

--- test/chatter_web/features/user_creates_new_chat_room_test.exs
+++ test/chatter_web/features/user_creates_new_chat_room_test.exs
@@ -2,15 +2,17 @@ defmodule ChatterWeb.UserCreatesNewChatRoomTest do
   use ChatterWeb.FeatureCase, async: true
 
   test "user creates a new chat room successfully", %{session: session}  do
+    user = insert(:user)
+
     session
-    |> visit(rooms_index())
+    |> visit("/")
+    |> sign_in(as: user)
     |> click(new_chat_link())
     |> create_chat_room(name: "elixir")
     |> assert_has(room_title("elixir"))
 
   end
 
-  defp rooms_index, do: Routes.chat_room_path(@endpoint, :index)
 
   defp new_chat_link, do: Query.link("New chat room")
 
@@ -22,6 +24,13 @@ defmodule ChatterWeb.UserCreatesNewChatRoomTest do
 
   defp room_title(title) do
     Query.data("role", "room-title", text: title)
+  end
+
+  defp sign_in(session, as: user) do
+    session
+    |> fill_in(Query.text_field("Email"), with: user.email)
+    |> fill_in(Query.text_field("Password"), with: user.password)
+    |> click(Query.button("Sign in"))
   end
 
 end
