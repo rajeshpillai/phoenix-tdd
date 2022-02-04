62bd4707bd5e653e045d55acb2c4c2d257de6401
rajeshpillai <pillai.rajesh@gmail.com>
2022-02-04 08:35:40 +0530

    Write a passing model test

--- lib/chatter/chat/room.ex
+++ lib/chatter/chat/room.ex
@@ -1,10 +1,16 @@
 defmodule Chatter.Chat.Room do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "chat_rooms" do
     field :name, :string
 
     timestamps()
+  end
+
+  def changeset(struct, attrs) do
+    struct
+    |> cast(attrs, [:name])
   end
 
 end
